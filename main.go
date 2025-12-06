73f665e9bf937efefcf2f4573a854f5e69f77f52
murrou13 <murrou13@gmail.com>
2025-12-06 03:23:48 +0200

    allow cors

--- main.go
+++ main.go
@@ -26,6 +26,17 @@ func main() {
 }
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
+	// Allow CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	// Handle preflight requests
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	rawURL := r.URL.Query().Get("url")
 	log.Printf("Proxying request for URL: %s", rawURL)
 	if rawURL == "" {
