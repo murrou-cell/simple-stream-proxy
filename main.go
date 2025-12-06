687530c95de118435e8b25079eebb214e18d7952
murrou13 <murrou13@gmail.com>
2025-12-06 03:51:13 +0200

    test ocet stream hls.key

--- main.go
+++ main.go
@@ -26,11 +26,15 @@ func main() {
 }
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
-	// Allow CORS
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
+	// if hls.key is requested do not use cors
+	if strings.HasSuffix(r.URL.Query().Get("url"), "hls.key") {
+		w.Header().Set("Content-Type", "application/octet-stream")
+	} else {
+		// Allow CORS
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	}
 	// Handle preflight requests
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
